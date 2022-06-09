ddebd5581b64bcd2ee24f4bbd7d31a49be47313a
Dimitris Zafeiris <kremou115@gmail.com>
2022-06-09 13:45:12 +0300

    Added Required decorators at Models> Student, Trainer

--- Prototype/Models/Trainer.cs
+++ Prototype/Models/Trainer.cs
@@ -15,24 +15,25 @@ namespace Prototype.Models
         public int TrainerId { get; set; }
 
         [Required]
-        [StringLength(50)]
+        [StringLength(50, MinimumLength = 5)]
         public string FirstName { get; set; }
 
         [Required]
-        [StringLength(70)]
+        [StringLength(70, MinimumLength = 5)]
         public string LastName { get; set; }
 
         [Required]
-        [StringLength(100)]
+        [StringLength(100, MinimumLength = 15)]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(20)]
+        [StringLength(20, MinimumLength = 10)]
         public string Phone { get; set; }
 
         [Required]
         public decimal Salary { get; set; }
 
+        [Range(15, 100)]
         public int Age { get; set; }
 
         [Required]
